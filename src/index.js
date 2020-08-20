f83e17e13442b396e1e71813529ace8e2f56c968
shuyang0129 <shuyang0129@gmail.com>
2020-08-20 22:01:40 +0800

    HTML and CSS for design

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './css/reset.css';
 import './css/index.css';
+import './css/reset.css';
 import App from './App';
-
-import { Provider } from 'react-redux';
 import store from './store.js';
-
-import { increment, decrement, asyncDecrement } from './actions/counterAction';
-
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(asyncDecrement());
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
