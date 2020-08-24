256e5e7e36c62d20d8738d58a6d882fea0f5d590
shuyang <shuyang.chou@hngf.com.tw>
2020-08-24 15:13:56 +0800

    Add directory alias

--- config-overrides.js
+++ config-overrides.js
@@ -6,9 +6,15 @@ const path = require('path');
 module.exports = function override(config, env) {
   config = rewireAliases.aliasesOptions({
     '@': path.resolve(__dirname, `${paths.appSrc}/`),
-    '@pages': path.resolve(__dirname, `${paths.appSrc}/pages/`),
     '@assets': path.resolve(__dirname, `${paths.appSrc}/assets/`),
+    '@img': path.resolve(__dirname, `${paths.appSrc}/img/`),
+    '@data': path.resolve(__dirname, `${paths.appSrc}/data/`),
+    '@constants': path.resolve(__dirname, `${paths.appSrc}/constants/`),
+    '@pages': path.resolve(__dirname, `${paths.appSrc}/pages/`),
     '@components': path.resolve(__dirname, `${paths.appSrc}/components/`),
+    '@reducers': path.resolve(__dirname, `${paths.appSrc}/reducers/`),
+    '@actions': path.resolve(__dirname, `${paths.appSrc}/actions/`),
+    '@utils': path.resolve(__dirname, `${paths.appSrc}/utils/`),
   })(config, env);
   return config;
 };
