ea2407c7e42e4c353831a5b5c01343f093d6ab02
shuyang0129 <shuyang0129@gmail.com>
2020-09-01 07:17:17 +0800

    Change naming

--- src/providers/CardContextProvider.jsx
+++ src/providers/CardContextProvider.jsx
@@ -57,7 +57,7 @@ import S12 from '@img/cards/S12.png';
 import S13 from '@img/cards/S13.png';
 
 // prettier-ignore
-const imgSrcs = {
+const imgSrc = {
   C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, C13,
   H1, H2, H3, H4, H5, H6, H7, H8, H9, H10, H11, H12, H13,
   D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13,
@@ -67,7 +67,7 @@ const imgSrcs = {
 export const CardContext = createContext();
 
 const CardContextProvider = ({ children }) => {
-  return <CardContext.Provider value={imgSrcs}>{children}</CardContext.Provider>;
+  return <CardContext.Provider value={imgSrc}>{children}</CardContext.Provider>;
 };
 
 export default CardContextProvider;
