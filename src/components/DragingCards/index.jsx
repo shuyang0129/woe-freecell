0a4741be81ddf854a4e9380054d39b1d5c10e8f6
shuyang <shuyang.chou@hngf.com.tw>
2020-08-31 16:24:16 +0800

    Move style in to file

--- src/components/DragingCards/index.jsx
+++ src/components/DragingCards/index.jsx
@@ -1,31 +1,12 @@
+import * as S from './style';
 import * as dndType from '@constants/dndType';
 
 import React, { useEffect, useState } from 'react';
 
 import { getSelectingCards } from '@/utils/freecell';
 import { renderCards } from '@utils/renderCard';
-import styled from 'styled-components';
 import { useDragLayer } from 'react-dnd';
 import { useSelector } from 'react-redux';
-
-const DragginCards = styled.div.attrs(({ initialOffset, currentOffset }) => {
-  let { x, y } = currentOffset;
-  return {
-    style: {
-      display: (!initialOffset || !currentOffset) && 'none',
-      transform: `translate(${x}px, ${y}px)`,
-    },
-  };
-})`
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 100;
-  & > :nth-child(n + 2) {
-    box-shadow: 0 -0.5px rgba(186, 186, 186, 0.2);
-    margin-top: -120px;
-  }
-`;
 
 const DraggingCards = () => {
   const game = useSelector(({ game }) => game);
@@ -52,6 +33,7 @@ const DraggingCards = () => {
   useEffect(() => {
     const absortController = new AbortController();
     const signal = absortController.signal;
+    console.log('signal', signal);
 
     return () => absortController.abort();
   }, []);
@@ -59,9 +41,9 @@ const DraggingCards = () => {
   if (!isDragging || !item || itemType !== dndType.CARD) return null;
 
   return (
-    <DragginCards initialOffset={initialOffset} currentOffset={currentOffset}>
+    <S.DragginCards initialOffset={initialOffset} currentOffset={currentOffset}>
       {renderCards(draggingCards, { sourceType: item.sourceType, sourceId: item.sourceId })}
-    </DragginCards>
+    </S.DragginCards>
   );
 };
 
---
+++ src/components/DragingCards/style.jsx
@@ -0,0 +1,20 @@
+const { default: styled } = require('styled-components');
+
+export const DragginCards = styled.div.attrs(({ initialOffset, currentOffset }) => {
+  let { x, y } = currentOffset;
+  return {
+    style: {
+      display: (!initialOffset || !currentOffset) && 'none',
+      transform: `translate(${x}px, ${y}px)`,
+    },
+  };
+})`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  & > :nth-child(n + 2) {
+    box-shadow: 0 -0.5px rgba(186, 186, 186, 0.2);
+    margin-top: -120px;
+  }
+`;
