57ce3f9491691f6e38627da98e5751ca23c5ccee
shuyang <shuyang.chou@hngf.com.tw>
2020-09-01 16:53:52 +0800

    Fishining custom drag layer

--- src/components/DragingCards/index.jsx
+++ src/components/DragingCards/index.jsx
@@ -1,42 +1,17 @@
 import * as S from './style';
 import * as dndType from '@constants/dndType';
 
-import React, { memo, useEffect, useState } from 'react';
+import React, { memo } from 'react';
 
-import Card from '@components/Card';
-import { getSelectingCards } from '@/utils/freecell';
-import { renderCards } from '@utils/renderCard';
+import { CardContext } from '../../providers/CardContextProvider';
+import { Card as StyledCard } from '@components/Card/style';
 import { uniqueId } from 'lodash';
+import { useContext } from 'react';
 import { useDragLayer } from 'react-dnd';
-import { useSelector } from 'react-redux';
-
-const layerStyles = {
-  position: 'fixed',
-  pointerEvents: 'none',
-  zIndex: 100,
-  left: 0,
-  top: 0,
-  width: '100%',
-  height: '100%',
-};
-
-const getItemStyles = (initialOffset, currentOffset) => {
-  if (!initialOffset || !currentOffset) {
-    return {
-      display: 'none',
-    };
-  }
-  let { x, y } = currentOffset;
-
-  const transform = `translate(${x}px, ${y}px)`;
-  return {
-    transform,
-    WebkitTransform: transform,
-  };
-};
 
 const DraggingCards = () => {
-  const [isRender, setIsRender] = useState(true);
+  const imgSrc = useContext(CardContext);
+
   const { item, itemType, isDragging, initialOffset, currentOffset } = useDragLayer(monitor => ({
     item: monitor.getItem(),
     itemType: monitor.getItemType(),
@@ -45,23 +20,17 @@ const DraggingCards = () => {
     isDragging: monitor.isDragging(),
   }));
 
-  // useEffect(() => {
-  //   if (!!item) console.log(item);
-  // }, [item]);
-
   if (!isDragging || itemType !== dndType.CARD) return null;
 
   return (
-    // <h3>Hello</h3>
-    <div style={layerStyles}>
-      {/* <S.DragginCards style={getItemStyles(initialOffset, currentOffset)}> */}
+    <S.DragginerLayerContainer>
       <S.DragginCards initialOffset={initialOffset} currentOffset={currentOffset}>
-        {/* {isRendered && item.dragItems.map(cardId => <Card key={uniqueId()} cardId={cardId} />)} */}
-        {!!item.dragItems && isRender && renderCards(['C1', 'H2'])}
+        {item.dragItems.map(cardId => (
+          <StyledCard key={uniqueId()} src={imgSrc[cardId]} alt="poker card" />
+        ))}
       </S.DragginCards>
-    </div>
+    </S.DragginerLayerContainer>
   );
 };
 
 export default memo(DraggingCards);
-//initialOffset={initialOffset} currentOffset={currentOffset}
--- src/components/DragingCards/style.jsx
+++ src/components/DragingCards/style.jsx
@@ -12,12 +12,20 @@ export const DragginCards = styled.div.attrs(({ initialOffset, currentOffset }) 
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
   pointer-events: none;
   z-index: 100;
   & > :nth-child(n + 2) {
     box-shadow: 0 -0.5px rgba(186, 186, 186, 0.2);
-    margin-top: -120px;
+    margin-top: -110px;
   }
+`;
+
+export const DragginerLayerContainer = styled.div`
+  position: 'fixed';
+  pointer-events: 'none';
+  z-index: 100;
+  left: 0;
+  top: 0;
+  width: '100%';
+  height: '100%';
 `;
