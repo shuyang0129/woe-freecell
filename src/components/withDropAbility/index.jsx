c9486210258b5fc1f686bab7dc634cbb50cbeccd
shuyang0129 <shuyang0129@gmail.com>
2020-09-03 21:22:00 +0800

    Improve withDropAbility hoc

--- src/components/withDropAbility/index.jsx
+++ src/components/withDropAbility/index.jsx
@@ -1,9 +1,10 @@
 import * as dndType from '@constants/dndType';
 
 import React from 'react';
-import { moveToFreeCell } from '../../actions/gameAction';
+import { moveToFreeCell, moveToFoundationCell, moveToTableau } from '../../actions/gameAction';
 import { useDispatch } from 'react-redux';
 import { useDrop } from 'react-dnd';
+import * as cells from '@constants/cells';
 
 const withDropAbility = Component => props => {
   const dispatch = useDispatch();
@@ -11,14 +12,23 @@ const withDropAbility = Component => props => {
   const [, drop] = useDrop({
     accept: dndType.CARD,
     drop: droppedItem => {
-      dispatch(
-        moveToFreeCell({
-          targetId: props.sourceId, // 要到哪個freeCell，拿取props傳進來的sourceId
-          cardId: droppedItem.cardId, // 是哪張卡片，取自droppedItem
-          sourceId: droppedItem.sourceId, // 來自哪個sourceId，取自droppedItem
-          sourceType: droppedItem.sourceType, // 來自哪個sourceType，取自droppedItem
-        }),
-      );
+      const params = {
+        targetId: props.sourceId, // 要到哪個freeCell，拿取props傳進來的sourceId
+        cardId: droppedItem.cardId, // 是哪張卡片，取自droppedItem
+        sourceId: droppedItem.sourceId, // 來自哪個sourceId，取自droppedItem
+        sourceType: droppedItem.sourceType, // 來自哪個sourceType，取自droppedItem
+      };
+
+      switch (props.sourceType) {
+        case cells.FREE_CELLS:
+          return dispatch(moveToFreeCell(params));
+        case cells.FOUNDATION_CELLS:
+          return dispatch(moveToFoundationCell(params));
+        case cells.TABLEAU:
+          return dispatch(moveToTableau(params));
+        default:
+          return;
+      }
     },
   });
 
