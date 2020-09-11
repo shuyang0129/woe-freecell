ed4b5180496ed1cca2987576c43cbc4818a73213
shuyang0129 <shuyang0129@gmail.com>
2020-09-12 00:51:43 +0800

    Create hoc for withHintForCell

--- src/components/FoundationCells/FoudationCell.jsx
+++ src/components/FoundationCells/FoudationCell.jsx
@@ -1,9 +1,9 @@
 import * as S from './style';
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { renderCards } from '@utils/renderCard';
 import withDropAbility from '@components/withDropAbility';
-import { useSelector } from 'react-redux';
+import withHintForCell from '@components/withHintForCell';
 
 /**
  * @name FoundationCell
@@ -13,26 +13,14 @@ import { useSelector } from 'react-redux';
  * @param {String} props.sourceId 這個FoundationCell的sourceId，Ex: CLUB
  * @description FoudationCell的獨立元件，方便使用useDrop這個Hooks，以及拖放的相關處理
  */
-const FoundationCell = React.forwardRef(({ foundationCellCards, sourceType, sourceId }, ref) => {
-  const [isShowHint, setIsShowHint] = useState(false);
-  const [isCellEmpty, setIsCellEmpty] = useState(true);
-  const { play, game } = useSelector(state => state);
+const FoundationCell = React.forwardRef(
+  ({ foundationCellCards, sourceType, sourceId, isShowHint, isCellEmpty }, ref) => {
+    return (
+      <S.FoundationCell suit={sourceId} isShowHint={isShowHint} isCellEmpty={isCellEmpty} ref={ref}>
+        {renderCards(foundationCellCards, { sourceType, sourceId })}
+      </S.FoundationCell>
+    );
+  },
+);
 
-  useEffect(() => {
-    // 如果 possibleMove 為 null，不做任何事
-    if (!play.possibleMove) return;
-    // 如果不是 possibleMove 中描述的targetId
-    if (play.possibleMove.targetId === sourceId && play.possibleMove.targetType === sourceType) {
-      setIsShowHint(true);
-      if (game[sourceType][sourceId].length) setIsCellEmpty(false);
-    }
-  }, [game, play.possibleMove, sourceId, sourceType]);
-
-  return (
-    <S.FoundationCell suit={sourceId} isShowHint={isShowHint} isCellEmpty={isCellEmpty} ref={ref}>
-      {renderCards(foundationCellCards, { sourceType, sourceId })}
-    </S.FoundationCell>
-  );
-});
-
-export default withDropAbility(FoundationCell);
+export default withHintForCell(withDropAbility(FoundationCell));
---
+++ src/components/withHintForCell/index.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+const withHintForCell = Component => props => {
+  const [isShowHint, setIsShowHint] = useState(false);
+  const [isCellEmpty, setIsCellEmpty] = useState(true);
+  const { play, game } = useSelector(state => state);
+
+  useEffect(() => {
+    const { sourceType, sourceId } = props;
+    // 如果 possibleMove 為 null，不做任何事
+    if (!play.possibleMove) return;
+    // 如果不是 possibleMove 中描述的targetId
+    if (play.possibleMove.targetId === sourceId && play.possibleMove.targetType === sourceType) {
+      setIsShowHint(true);
+      if (game[sourceType][sourceId].length) setIsCellEmpty(false);
+    }
+  }, [game, play.possibleMove, props]);
+
+  return <Component {...props} isShowHint={isShowHint} isCellEmpty={isCellEmpty} />;
+};
+
+export default withHintForCell;
