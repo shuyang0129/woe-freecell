54226e8c47b7470f0fcceca7839ed7e7f99a9645
shuyang0129 <shuyang0129@gmail.com>
2020-09-17 23:17:35 +0800

    Implement popup feature

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './style';
 import Logo from '@img/logo/king_joy.png';
+import cloeButtonImg from '@img/button/button_close.svg';
+import { useDispatch, useSelector } from 'react-redux';
+import { startNewGame, restartGame } from '@actions/gameAction';
+import { checkIsGameWin } from '@utils/freecell';
 
 const Popup = () => {
+  const [isShowPopup, setIsShowPopup] = useState(false);
+  const game = useSelector(({ game }) => game);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (checkIsGameWin(game)) setIsShowPopup(true);
+  }, [game, isShowPopup]);
+
+  if (!isShowPopup) return null;
+
+  const handleStartNewGame = () => {
+    dispatch(startNewGame());
+    setIsShowPopup(false);
+  };
+
+  const handleReStartNewGame = () => {
+    dispatch(restartGame());
+    setIsShowPopup(false);
+  };
+
   return (
     <S.PopupOverlay>
       <S.PopupContainer>
+        <S.CloseButton onClick={() => setIsShowPopup(false)}>
+          <img src={cloeButtonImg} alt="Close button" />
+        </S.CloseButton>
         <S.PopupContent>
           <S.Logo src={Logo} />
           <S.Title>congratulationS!</S.Title>
           <S.Description>You win the game!</S.Description>
           <S.ButtonContainer>
-            <S.Button>PLAY AGAIN</S.Button>
-            <S.Button>NEW GAME</S.Button>
+            <S.Button onClick={handleReStartNewGame}>PLAY AGAIN</S.Button>
+            <S.Button onClick={handleStartNewGame}>NEW GAME</S.Button>
           </S.ButtonContainer>
         </S.PopupContent>
       </S.PopupContainer>
--- src/components/Popup/style.jsx
+++ src/components/Popup/style.jsx
@@ -1,86 +1,92 @@
 import styled from 'styled-components';
 
 export const PopupOverlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: center;
   align-items: center;
   background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: center;
+  left: 0;
+  position: fixed;
+  top: 0;
+  width: 100%;
   z-index: 5;
 `;
 
 export const PopupContainer = styled.div`
-  min-width: 600px;
-  height: 330px;
+  align-items: center;
   background: #fcfcfc;
   border-radius: 10px;
   display: flex;
   flex-flow: column nowrap;
-  align-items: center;
+  height: 330px;
   justify-content: center;
+  min-width: 600px;
   padding: 50px;
+  position: relative;
 `;
 
 export const PopupContent = styled.div`
+  align-items: center;
   display: flex;
   flex-flow: column nowrap;
-  align-items: center;
   height: 100%;
 `;
 
 export const Logo = styled.img`
-  width: 60px;
   height: 60px;
   margin-bottom: 15px;
+  width: 60px;
 `;
 
 export const Title = styled.h2`
-  text-align: center;
+  color: #222222;
+  font-size: 20px;
   font-weight: bold;
   letter-spacing: 2px;
-  color: #222222;
-  text-transform: uppercase;
-  font-size: 20px;
   margin-bottom: 20px;
+  text-align: center;
+  text-transform: uppercase;
 `;
 
 export const Description = styled.p`
-  text-align: center;
-  letter-spacing: 0.7px;
   color: #333333;
   font-size: 14px;
+  letter-spacing: 0.7px;
+  text-align: center;
 `;
 
 export const Button = styled.button`
-  width: 150px;
-  height: 40px;
+  align-items: center;
   background: #222222;
   border-radius: 50px;
+  color: #fcfcfc;
   display: flex;
   flex-flow: row nowrap;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  font-weight: bold;
   font-size: 12px;
+  font-weight: bold;
+  height: 40px;
+  justify-content: center;
   letter-spacing: 1.2px;
-  color: #fcfcfc;
+  text-align: center;
+  width: 150px;
 `;
 
 export const ButtonContainer = styled.div`
+  align-items: center;
   display: flex;
   flex-flow: row nowrap;
-  align-items: center;
   margin-top: auto;
-
   & > :nth-child(n + 2) {
     margin-left: 15px;
   }
 `;
 
-export const CloseButton = styled.button``;
+export const CloseButton = styled.button`
+  height: 20px;
+  position: absolute;
+  right: 30px;
+  top: 30px;
+  width: 20px;
+`;
